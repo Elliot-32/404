13b8a10166a2c3b9052fcbb8d8f6c590f424bffe
Hacker-liu <115418645+Hacker-liu@users.noreply.github.com>
2023-10-26 21:32:09 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
-let Time = 1000
-while (input.runningTime() < 8000) {
+function 間隔 () {
+    if (input.runningTime() >= 8000) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    basic.pause(Time)
+}
+let Time = 0
+Time = 1000
+while (input.runningTime() < 10000) {
     basic.showLeds(`
         . . # . .
         . # # # .
@@ -7,7 +20,7 @@ while (input.runningTime() < 8000) {
         . # . # .
         . # . . #
         `)
-    basic.pause(Time)
+    間隔()
     basic.showLeds(`
         . . # . .
         . # # # .
@@ -15,8 +28,31 @@ while (input.runningTime() < 8000) {
         . # . # .
         . # . # .
         `)
-    basic.pause(Time)
+    間隔()
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . # . # .
+        # . . # .
+        `)
+    間隔()
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . # . # .
+        # . . # .
+        `)
+    間隔()
     if (input.runningTime() >= 5000) {
         Time = 0
     }
 }
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    # . # . #
+    . # . # .
+    . # . # .
+    `)
